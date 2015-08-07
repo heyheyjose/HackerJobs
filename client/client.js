093a38292f249fe2f7702a03fafe16a43ca66355
JosephShering <joseph@ctsfla.com>
2015-08-07 19:44:06 -0400

    Added hacker news api library

--- client/client.js
+++ client/client.js
@@ -1,14 +1,3 @@
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-	counter: function () {
-		return Session.get('counter');
-	}
-});
-
-Template.hello.events({
-	'click button': function () {
-		// increment the counter when button is clicked
-		Session.set('counter', Session.get('counter') + 1);
-	}
+Template.body.helpers({
+    jobs: Job.find({})
 });
---
+++ server/startup.js
@@ -0,0 +1,16 @@
+Meteor.startup(function () {
+    Job.remove({});
+	var api = new HackerNewsApi();
+    var kids = api.getItem('9996333').data.kids;
+
+    //For demo purposes it only gets the first 5 posts
+    for(var i = 0; i < 4; i++) {
+
+        var kid = kids[i];
+
+        api.getItem(kid, function(err, resp) {
+            Job.insert(resp.data);
+        });
+
+    }
+});
