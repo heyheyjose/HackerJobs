5d6fa53fde561f6f79d4f3d7de18599411fd94a9
JosephShering <joseph@ctsfla.com>
2015-08-08 16:34:40 -0400

    Added more parsing

--- client/client.js
+++ client/client.js
@@ -1,3 +1,11 @@
 Template.body.helpers({
     jobs: Job.find({})
 });
+
+Template.jobItem.helpers({
+    getStates: function () {
+        var stateCode = this.states[0];
+
+        return States[stateCode];
+    }
+});
