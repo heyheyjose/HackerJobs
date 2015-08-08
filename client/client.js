62827fb29ffeb40c0863abfc05f95e90d4fec1c1
JosephShering <joseph@ctsfla.com>
2015-08-08 18:19:26 -0400

    Added email to parse

--- client/client.js
+++ client/client.js
@@ -5,14 +5,23 @@ Template.body.helpers({
 Template.jobItem.helpers({
     getStates: function () {
         var stateCode = this.states[0];
-
         return States[stateCode];
     }
 });
 
 Template.jobItem.events({
     'click #toggle-post': function(e) {
-        $(this).find('span').text('Hide');
-        $('.raw-text').toggle();
+        var $a = $(e.currentTarget);
+
+        var text = $a.find('span').text();
+
+        if(text === 'Hide') {
+            $a.find('span').text('Show');
+        }
+        else {
+            $a.find('span').text('Hide');
+        }
+
+        $a.siblings('.raw-text').toggle();
     }
 });
