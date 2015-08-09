57cd232dc99757b28fb439768d8775406072bed8
JosephShering <joseph@ctsfla.com>
2015-08-08 21:41:12 -0400

    added more states

--- server/startup.js
+++ server/startup.js
@@ -1,13 +1,12 @@
 Meteor.startup(function () {
     Job.remove({});
-    Language.remove({});
 
     var api = new HackerNewsApi();
 
     function getJobs() {
         var kids = api.getItem('9996333').data.kids;
 
-        for (var i = 0; i < 3; i++) {
+        for (var i = 0; i < kids.length; i++) {
             var kid = kids[i];
 
             api.getItem(kid, function (err, resp) {
@@ -21,7 +20,9 @@ Meteor.startup(function () {
         }
     }
 
-    getJobs();
+    if (Job.find({}).fetch().length === 0) {
+        getJobs();
+    }
 
     if (Language.find({}).fetch().length === 0) {
 
@@ -29,15 +30,13 @@ Meteor.startup(function () {
         Language.insert(skillList);
     }
 
+    SyncedCron.add({
+        name: 'Gets all of the information from the hacker news posts',
+        schedule: function (parser) {
+            return parser.text('every 1 minute');
+        },
+        job: getComments
+    });
 
-
-    /*SyncedCron.add({
-     name: 'Gets all of the information from the hacker news posts',
-     schedule: function (parser) {
-     return parser.text('every 1 minute');
-     },
-     job: getComments
-     });*/
-
-    //SyncedCron.start();
+    SyncedCron.start();
 });
